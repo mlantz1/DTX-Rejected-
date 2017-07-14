5b1b0c34e67b67ba709d91a6c71dbfc389cad228
Jared Dembrun <jared.dembrun@ge.com>
2017-07-14 10:20:50 -0700

    Partly formatted json to send to front end.

--- app.py
+++ app.py
@@ -1,26 +1,42 @@
 #Get the data from all turbines.
 import requests
+import json
 from flask import Flask, render_template
 
 #create the application instance
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-	return render_template('display_data.html')
-
-#Return
-@app.route("/getdatastring")
+#Return all turbine data
+@app.route("/getAllData")
 def getCurrentData():
 	#Use https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/:turbine_id/sensors/:sensor_id
 	turbine1_temp = requests.get("https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/1/sensors/temperature")
 	turbine1_volt = requests.get("https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/1/sensors/voltage")
-	turbine2_temp = requests.get("https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/2/sensors/temperature")
-	turbine2_volt = requests.get("https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/2/sensors/voltage")
-	turbine3_temp = requests.get("https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/3/sensors/temperature")
-	turbine3_volt = requests.get("https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/3/sensors/voltage")
+	turbine1_stat = requests.get("https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/1/heartbeat")
 
-	data = '{' + turbine1_temp.content + turbine1_volt.content + turbine2_temp.content + turbine2_volt.content + turbine3_temp.content + turbine3_volt.content + '}'
+	data = turbine1_temp.data()
+	return data
+
+#Return one turbine's data as a string
+@app.route("/getTurbineData/<id>/")
+def getTurbineDataString(id):
+	return str(getTurbineData(id))
+
+#Return one turbine's data
+def getTurbineData(id):
+	#Get data
+	turbine_temp = requests.get("https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/" + str(id) + "/sensors/temperature")
+	turbine_volt = requests.get("https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/" + str(id) + "/sensors/voltage")
+	turbine_stat = requests.get("https://turbine-farm.run.aws-usw02-pr.ice.predix.io/api/turbines/" + str(id) + "/heartbeat")
+	data = dict()
+
+	#Populate Dictionary
+	data["id"] = str(id)
+	data["timestamp"] = json.loads(turbine_temp.text)['timestamp']
+	data["temp"] = json.loads(turbine_temp.text)['value']
+	data["voltage"] = json.loads(turbine_volt.text)['value']
+	data["status"] = json.loads(turbine_stat.text)['status']
+
 	return data
 
 #Get the data in a list instead of JSON
